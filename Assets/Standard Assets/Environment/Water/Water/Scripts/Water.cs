0ae3dbc8b2a233d658892185b8a8f4295e1e202b
do.hoang.than <do.hoang.than@sun-asterisk.com>
2023-05-29 10:54:03 +0700

    return to one standard asset

--- Assets/Standard Assets/Environment/Water/Water/Scripts/Water.cs
+++ Assets/Standard Assets/Environment/Water/Water/Scripts/Water.cs
@@ -187,34 +187,35 @@ namespace UnityStandardAssets.Water
 
         // This just sets up some matrices in the material; for really
         // old cards to make water texture scroll.
-        void Update()
-        {
-            if (!GetComponent<Renderer>())
-            {
-                return;
-            }
-            Material mat = GetComponent<Renderer>().sharedMaterial;
-            if (!mat)
-            {
-                return;
-            }
+        // Don't use it. I hate it.
+        //void Update()
+        //{
+        //    if (!GetComponent<Renderer>())
+        //    {
+        //        return;
+        //    }
+        //    Material mat = GetComponent<Renderer>().sharedMaterial;
+        //    if (!mat)
+        //    {
+        //        return;
+        //    }
 
-            Vector4 waveSpeed = mat.GetVector("WaveSpeed");
-            float waveScale = mat.GetFloat("_WaveScale");
-            Vector4 waveScale4 = new Vector4(waveScale, waveScale, waveScale * 0.4f, waveScale * 0.45f);
+        //    Vector4 waveSpeed = mat.GetVector("WaveSpeed");
+        //    float waveScale = mat.GetFloat("_WaveScale");
+        //    Vector4 waveScale4 = new Vector4(waveScale, waveScale, waveScale * 0.4f, waveScale * 0.45f);
 
-            // Time since level load, and do intermediate calculations with doubles
-            double t = Time.timeSinceLevelLoad / 20.0;
-            Vector4 offsetClamped = new Vector4(
-                (float)Math.IEEERemainder(waveSpeed.x * waveScale4.x * t, 1.0),
-                (float)Math.IEEERemainder(waveSpeed.y * waveScale4.y * t, 1.0),
-                (float)Math.IEEERemainder(waveSpeed.z * waveScale4.z * t, 1.0),
-                (float)Math.IEEERemainder(waveSpeed.w * waveScale4.w * t, 1.0)
-                );
+        //    // Time since level load, and do intermediate calculations with doubles
+        //    double t = Time.timeSinceLevelLoad / 20.0;
+        //    Vector4 offsetClamped = new Vector4(
+        //        (float)Math.IEEERemainder(waveSpeed.x * waveScale4.x * t, 1.0),
+        //        (float)Math.IEEERemainder(waveSpeed.y * waveScale4.y * t, 1.0),
+        //        (float)Math.IEEERemainder(waveSpeed.z * waveScale4.z * t, 1.0),
+        //        (float)Math.IEEERemainder(waveSpeed.w * waveScale4.w * t, 1.0)
+        //        );
 
-            mat.SetVector("_WaveOffset", offsetClamped);
-            mat.SetVector("_WaveScale4", waveScale4);
-        }
+        //    mat.SetVector("_WaveOffset", offsetClamped);
+        //    mat.SetVector("_WaveScale4", waveScale4);
+        //}
 
         void UpdateCameraModes(Camera src, Camera dest)
         {
